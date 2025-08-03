73bcdcfd75d02ac9e08af4b71565911fc466a3e4
Ziaur Rahman <programmerzia@gmail.com>
2025-08-03 16:17:03 +0600

    fix

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -55,7 +55,7 @@ definePageMeta({
                 <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div class="rounded-md shadow">
                     <NuxtLink
-                      to="/register"
+                      to="#"
                       class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                     >
                       Get started
